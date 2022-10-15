d58c5badea33c7813e08d85a3add9941de844693
merhan-mohamed <merhan_mohamed89@hotmail.com>
2022-10-15 22:17:56 +0200

    update18

--- src/components/About/style.js
+++ src/components/About/style.js
@@ -4,6 +4,6 @@ background-position:center;
 background-size:cover;
 height:815px;
 position:relative;
-background:url("/images/About/about-bg.jpg");
+background:url("./React-Proj//images/About/about-bg.jpg");
 `
 
