dcb22b4a1e686fb6da0a141d365753d6ec7c275c
merhan-mohamed <merhan_mohamed89@hotmail.com>
2022-10-15 22:45:42 +0200

    update22

--- src/components/Portofolio/portofolio.js
+++ src/components/Portofolio/portofolio.js
@@ -16,7 +16,7 @@ const Portofolio = () => {
     return(
         <div key={item.id}>
             <Imagewrapper>
-                <img src="{item.image}" className="d-block w-100 mt-2" alt="..." />
+                <img src="images/Portofolio/{item.image}" className="d-block w-100 mt-2" alt="..." />
                 <Overlay className="overlay">
                     <span>Show Image</span>
                 </Overlay>
@@ -53,7 +53,7 @@ const Portofolio = () => {
 
             <div>
                 <div className="grid">
-                    console.log({myImg})
+                    {myImg}
                 </div>
                 
             </div>
